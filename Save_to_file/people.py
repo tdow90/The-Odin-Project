54ca532587f4075fca30f468c781bcb9a97e7071
Tyler Downing <tyler.ar.downing@hotmail.com>
2024-12-23 15:32:43 -0400

    Added some additional logic and the load data function to my csv practice

--- Save_to_file/people.py
+++ Save_to_file/people.py
@@ -1,12 +1,29 @@
+from pathlib import Path
+
 people = [{"name": "Alice", "age": 30}, {"name": "Bob", "age": 25}, {"name": "Tyler", "age": 34}]
 
 def save_data(filename, dic):
     with open(filename, 'w') as data:
-        for dics in dic:
-            for key, value in dics.items():
-                data.write(f"{key}, {value}\n")
+       for item in dic:
+        values = ", ".join(str(value) for value in item.values())
+        data.write(f"{values}\n")
 
 
-save_data('people.txt', people)
+#save_data('people.txt', people)
 
 # Need to make a function to load data back into the dictionary
+def load_data(filename):
+    file_path = Path(filename)
+    if not file_path.exists():
+        print(f"Error: The file '{filename}' does not exist.")
+    
+    people = []
+    with open(filename, 'r') as data:
+        for line in data:
+            name, age = line.strip().split(", ")
+            people.append({"name": name, "age": age })
+    return people
+
+
+print(load_data('humans.txt'))
+
